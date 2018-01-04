699d2ae3b5cdfe1d4036776c8d81d5347bf0a3de
curry <17828455424@163.com>
2018-01-04 18:44:15 +0800

    git study again

--- git_note.js
+++ git_note.js
@@ -127,3 +127,22 @@
 /*
 * 要学习的 tag：不可移动的 branch、cherry-pick：把选中的 commits 一个个合并进来、git config： Git 的设置、Git Flow：复杂又高效的工作流
 * */
+/**
+ * 2018/1/4
+ * 场景：就是在git 监视的项目上，加了一个 .idea ，想 删除 当前 工作目录的 修改
+ *
+ # 删除 untracked files
+ git clean -f
+
+ # 连 untracked 的目录也一起删掉
+ git clean -fd
+
+ # 连 gitignore 的untrack 文件/目录也一起删掉 （慎用，一般这个是用来删掉编译出来的 .o之类的文件用的）
+ git clean -xfd
+
+ # 在用上述 git clean 前，墙裂建议加上 -n 参数来先看看会删掉哪些文件，防止重要文件被误删
+ git clean -nxfd
+ git clean -nf
+ git clean -nfd
+ *
+ */
